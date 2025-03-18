2ccac29d14999443c1693442d4a6e1cbd6941355
xu1feng <2465549609@qq.com>
2025-03-18 20:58:34 +0800

    feat: 代码评审 v4.1

--- openai-code-review-test/src/test/java/edu/xyf/middleware/test/ApiTest.java
+++ openai-code-review-test/src/test/java/edu/xyf/middleware/test/ApiTest.java
@@ -13,7 +13,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("aaaa1"));
+        System.out.println(Integer.parseInt("aaaa1234"));
         System.out.println(Integer.parseInt("aaaa2"));
         System.out.println(Integer.parseInt("aaaa3"));
     }
