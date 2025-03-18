842dce2bfd85b04fcd1a8d14a363bd7fb37fc78a
xu1feng <2465549609@qq.com>
2025-03-18 20:56:50 +0800

    feat: 代码评审

--- openai-code-review-sdk/src/test/java/edu/xyf/middleware/sdk/test/ApiTest.java
+++ openai-code-review-sdk/src/test/java/edu/xyf/middleware/sdk/test/ApiTest.java
@@ -1,9 +1,75 @@
 package edu.xyf.middleware.sdk.test;
 
 
+import com.alibaba.fastjson2.JSON;
+import edu.xyf.middleware.sdk.domain.model.ChatCompletionSyncResponse;
+import edu.xyf.middleware.sdk.types.utils.BearerTokenUtils;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class ApiTest {
 
     public static void main(String[] args) {
+        String apiKeySecret = "16fc1769f37e48c98ef62eff59841ab2.eXLJ84FORYKT3bj7";
+        String token = BearerTokenUtils.getToken(apiKeySecret);
+        System.out.println(token);
+    }
+
+    @Test
+    public void test_http() throws IOException {
+        String apiKeySecret = "16fc1769f37e48c98ef62eff59841ab2.eXLJ84FORYKT3bj7";
+        String token = BearerTokenUtils.getToken(apiKeySecret);
+
+        URL url = new URL("https://open.bigmodel.cn/api/paas/v4/chat/completions");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Authorization", "Bearer " + token);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+        connection.setDoOutput(true);
+
+        String code = "1+1";
+
+        String jsonInpuString = "{"
+                + "\"model\":\"glm-4-flash\","
+                + "\"messages\": ["
+                + "    {"
+                + "        \"role\": \"user\","
+                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + code + "\""
+                + "    }"
+                + "]"
+                + "}";
+
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = jsonInpuString.getBytes(StandardCharsets.UTF_8);
+            os.write(input);
+        }
+
+        int responseCode = connection.getResponseCode();
+        System.out.println(responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+
+        in.close();
+        connection.disconnect();
+
+        ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
+        System.out.println(response.getChoices().get(0).getMessage().getContent());
 
     }
 
