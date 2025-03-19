de08f304e56cbcf1bffdf8c69aa2667849953730
xu1feng <2465549609@qq.com>
2025-03-19 20:54:50 +0800

    feat: 评审消息模板通知 v6.1

--- openai-code-review-test/src/test/java/edu/xyf/middleware/test/ApiTest.java
+++ openai-code-review-test/src/test/java/edu/xyf/middleware/test/ApiTest.java
@@ -13,7 +13,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("abcddd1234"));
+        System.out.println(Integer.parseInt("a1234"));
     }
 
 }
