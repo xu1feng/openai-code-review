7a2fc3569f8e0fc55c94b2374c90a75271520d5d
xu1feng <2465549609@qq.com>
2025-03-20 17:17:57 +0800

    feat: 工程重构，增加可维护性 v7.3

--- openai-code-review-test/src/test/java/edu/xyf/middleware/test/ApiTest.java
+++ openai-code-review-test/src/test/java/edu/xyf/middleware/test/ApiTest.java
@@ -13,7 +13,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("12345"));
+        System.out.println(Integer.parseInt("abc12345"));
     }
 
 }
